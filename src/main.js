bdc2cbe00f1949723a2fcf0729d3ed8bd1db802c
developanther17 <developanther17@gmail.com>
2025-12-27 22:52:35 -0600

    motion_adds

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
-let menuBurguir = document.getElementById('burguir');
-let menu = document.getElementById('menu')
-let arrowMenuClose = document.getElementById('close-menu')
-
+const menuBurguir = document.getElementById('burguir');
+const menu = document.getElementById('menu');
+const arrowMenuClose = document.getElementById('close-menu');
 
 menuBurguir.addEventListener('click', (evento) => {
   evento.preventDefault();
   menuBurguir.classList.toggle('hidden');
-  menu.classList.toggle('hidden')
-  arrowMenuClose.classList.toggle('hidden')
-}
-)
+  menu.classList.toggle('hidden');
+  arrowMenuClose.classList.toggle('hidden'); 
+}); 
 
 arrowMenuClose.addEventListener('click', (event) => {
   event.preventDefault();
-  arrowMenuClose.classList.toggle('hidden')
+  arrowMenuClose.classList.toggle('hidden');
   menu.classList.toggle('hidden');
-  menuBurguir.classList.toggle('hidden');
-})
-
-
+  menuBurguir.classList.toggle('hidden'); 
+}); 
